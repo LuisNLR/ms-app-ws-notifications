1ab264298fbea5d1642e1b3018a6b1b486bad65b
noe Lizana <luisnoe6@hotmail.com>
2024-12-23 16:22:05 -0500

    update add task notification

--- src/main/java/pe/com/softlite/notifications/scheduleds/ScheduledTask.java
+++ src/main/java/pe/com/softlite/notifications/scheduleds/ScheduledTask.java
@@ -1,16 +1,23 @@
 package pe.com.softlite.notifications.scheduleds;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.util.Date;
 
 import pe.com.softlite.notifications.controller.NotificacionsController;
 
 @Component
 @EnableScheduling
 public class ScheduledTask {
+	
+	public final static Logger LOGGER = LoggerFactory.getLogger(ScheduledTask.class);
 	
 	@Autowired
 	private NotificacionsController controller;
@@ -23,9 +30,12 @@ public class ScheduledTask {
 	// Se ejecutará De lunes a vierntes (1-5), a horas 8 y 12, con 30 minutos.
 	@Scheduled(cron = "0 30 08,12 ? * 1-5")
 	public void taskNotification() {
+		String correlationId = UUID.randomUUID().toString();
+		LOGGER.info(correlationId + ":::: Proceso notificacion programada. Inicio :::: '{}' ", ScheduledTask.class.getName());
+		LOGGER.info(correlationId + ":::: Proceso notificacion programada. Hora: :::: '{}' ", new Date());
 		ResponseEntity<String> result = controller.sendNotifications();
-		System.out.println(result.getBody());
-		System.out.println("Ejecutando scheduling...");
+		LOGGER.info(correlationId + ":::: Proceso notificacion programada. Resultado: :::: '{}' ", result);
+		LOGGER.info(correlationId + ":::: Proceso notificacion programada. Fin :::: '{}' ", "Hecho");
 	}
 	
 }
